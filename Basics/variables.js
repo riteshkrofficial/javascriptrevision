049864fb096e6a57bb9f44b9a9afee56a1aab873
Ritesh Kumar <98106470+riteshkrofficial@users.noreply.github.com>
2024-01-10 12:48:05 +0000

    variables and scope basics

---
+++ Basics/variables.js
@@ -0,0 +1,11 @@
+const hey = 25644
+let accessEmail='riteshg500@gmail.com'
+var accessPassword = 435655
+accountCity='pinkcity'  //not a good practice although this line can also run in js 
+// console.log(accessEmail)
+let useState;
+console.table([hey,accessEmail,accessPassword,accountCity,useState])
+
+
+//use of let and const is good 
+//use of var create much errors in the file as var can be re-declared with same variable without showing any error 
