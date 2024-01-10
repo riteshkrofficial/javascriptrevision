5d6f47ad6992c1f7b57d5ecf6e04351115d744e3
Ritesh Kumar <98106470+riteshkrofficial@users.noreply.github.com>
2024-01-10 13:13:50 +0000

    datatype

---
+++ Basics/datatypes.js
@@ -0,0 +1,12 @@
+//let us see the datatypes here 
+
+let isAvailabe = true;
+console.log(isAvailabe);
+
+// null-> it means no value that is suppose you do not know about a particular things then it null value can be used in its place 
+// undefined -> it means you declared the data its other thing that you do not know the measuring scale but data is their
+
+let state =null;
+//important 
+console.log(typeof null);   //null is an object
+console.log(typeof undefined); // its is undefined type 
